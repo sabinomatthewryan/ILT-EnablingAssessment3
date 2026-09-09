8a99642602e52c80728e9c54c1139c57c7103f1b
Matthew Sabino <sabinomatthewryan@gmail.com>
2026-09-09 13:06:30 +0800

    feat: add Sabino component and mount in main application

--- TraderJoes/src/app/app.ts
+++ TraderJoes/src/app/app.ts
@@ -2,9 +2,10 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatIconModule } from '@angular/material/icon';
 import { Lim } from './lim/lim';
+import { Sabino } from './sabino/sabino';
 
 @Component({
-  imports: [RouterOutlet, Lim, MatIconModule],
+  imports: [RouterOutlet, Lim, Sabino, MatIconModule],
   selector: 'app-root',
   styleUrl: './app.scss',
   templateUrl: './app.html',
