df62c33f1722ed007d739b23aa903706b96c4206
Delacruz-Nicko <nickodelacruz8@gmail.com>
2026-09-09 13:48:37 +0800

    feat(delacruz): generate component and add to AppComponent

---
+++ TraderJoes/src/app/delacruz/delacruz.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-delacruz',
+  styleUrl: './delacruz.scss',
+  templateUrl: './delacruz.html',
+})
+export class Delacruz {}
