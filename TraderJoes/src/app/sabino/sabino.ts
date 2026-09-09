e6b65803e7d0d4d3f83ca216a7e09ad3f7e0e08f
Matthew Sabino <sabinomatthewryan@gmail.com>
2026-09-09 13:11:13 +0800

    feat(sabino): display member name using Angular signal

--- TraderJoes/src/app/sabino/sabino.ts
+++ TraderJoes/src/app/sabino/sabino.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   imports: [],
@@ -6,4 +6,6 @@ import { Component } from '@angular/core';
   styleUrl: './sabino.scss',
   templateUrl: './sabino.html',
 })
-export class Sabino {}
+export class Sabino {
+  readonly myName = signal('Matthew Sabino')
+}
