5894cd9b597f39e22c94778864adadff1605696f
Matthew Sabino <sabinomatthewryan@gmail.com>
2026-09-15 20:26:34 +0800

    feat(fix)/fix capitalization

--- TraderJoes/src/app/sabino/sabino.ts
+++ TraderJoes/src/app/sabino/sabino.ts
@@ -11,7 +11,7 @@ export class Sabino {
   readonly name = 'Matthew Ryan Sabino';
   readonly imagePath = 'assets/sabino.jpeg';
   readonly role = '4th Year IT Student • Web Development';
-  readonly skills = ['Angular', 'Typescript', 'Node.js'];
+  readonly skills = ['Angular', 'TypeScript', 'Node.js'];
   readonly bio =
     'Hi, Im Matthew, an aspiring software engineer passionate about ' +
     'modern web technologies. I love crafting responsive interfaces, solving ' +
